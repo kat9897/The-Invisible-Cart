586930fecacbe1c23ac45718554cab133f1bb3ac
Katrina Best <katrinabest@hotmail.com>
2021-11-17 11:58:21 -0500

    Adding beginning files

---
+++ app/src/main/java/com/example/b07_final_project/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.b07_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //hello test for git
+    }
+}
