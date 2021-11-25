07d1b31ece0eb3730cc9ccdaeab20beb7c4caeb1
ATeeter-UTSC <76921763+ATeeter-UTSC@users.noreply.github.com>
2021-11-25 01:07:55 -0500

    Finalized main dashboard for customers (should be in MainActivity.java and activity_main.xml)

--- app/src/main/java/com/example/b07_final_project/MainActivity.java
+++ app/src/main/java/com/example/b07_final_project/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.b07_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //Declare all the tools in which you want to assign functionality
+
+    private Button order_button, shop_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +20,39 @@ public class MainActivity extends AppCompatActivity {
         // Hide TitleBar
         getSupportActionBar().hide();
 
-        //hello test for git
+        order_button = findViewById(R.id.customer_order_button);
+        shop_button = findViewById(R.id.customer_shop_button);
+
+
+
+        // Get ownerid to check who logged in
+        Intent intent = getIntent();
+        String uid = intent.getExtras().getString("Userid");
+
+
+        // When this button is clicked perform the following task
+        order_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Open up another activity
+                //Intent intent1 = new Intent(MainActivity.this, MainActivity.class);
+                // send this ownerid to the activity
+//                intent1.putExtra("Userid", uid);
+//                startActivity(intent);
+            }
+        });
+
+        shop_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Open up another activity
+//                Intent intent1 = new Intent(MainActivity.this, MainActivity.class);
+//                intent1.putExtra("Userid", uid);
+//                // send this ownerid to the activity
+//                startActivity(intent);
+            }
+        });
+
     }
     @Override
     public void finish() {
