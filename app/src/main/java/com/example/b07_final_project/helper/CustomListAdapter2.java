9e9350b48268417653d206e8da7f74050c2a5bd4
theor <theoragavrila@gmail.com>
2021-12-01 15:37:59 -0500

    Added an alternative to AllProducts page for Customer

--- app/src/main/java/com/example/b07_final_project/helper/CustomListAdapter2.java
+++ app/src/main/java/com/example/b07_final_project/helper/CustomListAdapter2.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 
 import com.example.b07_final_project.R;
